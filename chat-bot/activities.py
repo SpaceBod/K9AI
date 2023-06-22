64d6443acae3a135e8e5a40599636547f4c06332
hamedm15 <84027928+hamedm15@users.noreply.github.com>
2023-06-22 16:17:47 +0100

    update

--- chat-bot/activities.py
+++ chat-bot/activities.py
@@ -90,3 +90,53 @@ def get_activity(text):
         if attempt_counter >= max_attempts:
             play_sound("sound/noSuggestions.mp3", 0.5, blocking=False)
         break  # Exit the outer while loop
+
+
+prev_mental_support_row = None
+prev_mental_support_response = None
+def mental_support(text):
+    global prev_mental_support_row
+    global prev_mental_support_response
+
+    # List of keywords related to depression or suicide
+    depression_keywords = ["depressed", "depression","die", "dark place", "hopeless", "despair", "suicide","suicidal", "kill myself", "don't want to live"]
+    helpline_responses =  ["Sorry to hear you're feeling this way. Your feelings are valid. Talk to someone you trust. Seek support for your well-being.",
+        "It's tough to hear you're going through this. You matter. Open up to a trusted person. Seek support in your journey.",
+        "I'm here for you. Your emotions are valid. Reach out to someone who cares. Seek the support you deserve.",
+        "I'm truly sorry you're feeling like this. Remember, you're not alone. Seek help from someone you trust. Take steps towards support.",
+        "You're not alone in this journey. It's okay to ask for help. Share with someone you trust. Seek support to navigate through.",
+        "I'm here to support you. Your strength is remarkable. Reach out to someone you trust. You deserve care and assistance.",
+        "You matter. Your emotions are valid. Share your struggles with someone you trust. Seek support for your well-being.",
+        "My heart goes out to you. Remember, you're stronger than you think. Lean on someone you trust. Seek support on your journey.",
+        "You deserve love and support. Your feelings are valid. Share your burden with someone you trust. Together, we can overcome.",
+        "I'm sorry to hear you're going through a tough time. Reach out to someone you trust. You're not alone. Seek support for your well-being."]
+    samaritans_helpline = "Don't hesitate to reach out to the Samaritans helpline by calling 116 123, available 24 7"
+    if any(keyword in text for keyword in depression_keywords):
+        if prev_mental_support_response:
+            helpline_responses.remove(prev_mental_support_response)
+        selected_response = random.choice(helpline_responses)
+        prev_mental_support_response = selected_response
+        speak(selected_response) # change to mp3
+        speak(samaritans_helpline) # change to mp3
+    else:
+        with open("assets/behaviours/Mental_Support.txt", "r") as file:
+            rows = file.readlines()
+            rows = [row.strip() for row in rows]
+            if prev_mental_support_row:
+                rows.remove(prev_mental_support_row)
+            selected_row = random.choice(rows)
+            prev_mental_support_row = selected_row
+            speak(selected_row)  # Replace this line with your preferred method of output
+        
+prev_love_you_response = None
+def love_you(text):
+    global prev_love_you_response
+    with open("assets/behaviours/Love_You.txt", "r") as file:
+        rows = file.readlines()
+        rows = [row.strip() for row in rows]
+        if prev_love_you_response:
+            rows.remove(prev_love_you_response)
+        selected_row = random.choice(rows)
+        prev_love_you_response = selected_row
+        speak(selected_row)  # Replace this line with your preferred method of output
+    
