ec14dbed4b56336cfc6ee423ba7fedd8fadec0af
Luca <lucabod8@gmail.com>
2023-07-03 16:04:32 +0100

    Tidy

--- chat-bot/main.py
+++ chat-bot/main.py
@@ -5,35 +5,35 @@ from watson import WatsonAssistant
 import json
 import pvporcupine
 import pyaudio
-import os
 from multiprocessing import Manager
 from movement.control_quadruped import main_quad
 from movement.quadruped import raise_head, lower_head
 import time
 import threading
 
-# Rest of your code...
-
+# Separate process for movement
 def second_file_process(sit):
     main_quad(sit)
 
-def initialize_chatbot():
+# Initialise the chatbot components
+def Initialise_chatbot():
     recognized_names = []
-    # Read the settings from the file
+
+    # Read the API settings from the file
     with open('settings.json') as f:
         settings = json.load(f)
-    # Extract the variables
     spotify_settings = settings['spotify']
     watson_assistant_settings = settings['watson_assistant']
     wake_word_settings = settings['porcupine_wake_word']
-    # Initialising chat bot
+
+    # Initialise speech recognition and audio output
     recogniser = speech_recognition.Recognizer()
-    print("got recogniser")
     calibrate_mic(recogniser)
-    print("calibrated mic")
+    print("Mic Calibrated")
     initialise_tts()
     pygame.init()
-    # Initialising Spotify
+
+    # Initialise Spotify
     spot, dev_ID = initialise_spotify(
         client_id=spotify_settings['client_id'],
         client_secret=spotify_settings['client_secret'],
@@ -44,6 +44,8 @@ def initialize_chatbot():
     )
     send_variables(spot, dev_ID)
     play_sound("sound/startup.mp3", 1, blocking=False)
+
+    # Scan faces and greet recognized individuals
     recognized_names = scan_face()
     for name in recognized_names:
         if name == "N" or name == "Unknown":
@@ -51,7 +53,7 @@ def initialize_chatbot():
         else:
             speak(f"Hi {name}. I'm K9.")
     play_sound("sound/help.mp3", 1, blocking=False)
-    
+
     # Create an instance of the WatsonAssistant class
     assistant = WatsonAssistant(
         api_key=watson_assistant_settings['api_key'],
@@ -61,7 +63,8 @@ def initialize_chatbot():
     )
     return recogniser, assistant, wake_word_settings
 
-def initialize_porcupine(wake_word_settings):
+# Initialise Porcupine wake word engine
+def Initialise_porcupine(wake_word_settings):
     access_key = wake_word_settings['api_key']
     custom_keyword_path = wake_word_settings['model_path']
     sensitivity = 0.7
@@ -78,27 +81,31 @@ def initialize_porcupine(wake_word_settings):
     )
     return porcupine, audio_stream
 
+# Main function
 def main(shared_list):
-    recogniser, assistant, wake_word_settings = initialize_chatbot()
-    porcupine, audio_stream = initialize_porcupine(wake_word_settings)
+    recogniser, assistant, wake_word_settings = Initialise_chatbot()
+    porcupine, audio_stream = Initialise_porcupine(wake_word_settings)
     second_thread = threading.Thread(target=second_file_process, args=(shared_list,))
     second_thread.start()
     try:
         while True:
+            # Wait for wake word "Hey K9"
             listen_for_wake_word(porcupine, audio_stream)
             raise_head()
+            # Pauses Spotify to listen to request clearly
             if is_music_paused():
                 pause_music('Pause')
                 time.sleep(3)
             play_sound("sound/ready.mp3", 1, blocking=False)
             try:
+                # Listens to the user and transfroms speech to text
                 user_input = recognise_input(recogniser)
                 print(f"[INPUT]\t{user_input}")
+                # Passes the text to Watson
                 assistant.watson_chat(user_input, shared_list)
             except speech_recognition.UnknownValueError:
                 recogniser = speech_recognition.Recognizer()
                 play_sound("sound/repeat.mp3", 1, blocking=True)
-            # Check process using the speaker device
             lower_head()
     finally:
         second_thread.join()
@@ -106,6 +113,7 @@ def main(shared_list):
             porcupine.delete()
 
 if __name__ == '__main__':
+    # Create variables to be shared across processes
     manager = Manager()
     shared_list = manager.list([False, False])
     main(shared_list)
