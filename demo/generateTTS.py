9daa0b60fd10160010a0d88804bac0e40a83462d
Gian-Luca Fenocchi <lucabod8@gmail.com>
2023-07-18 19:00:57 +0100

    Updated Voices

--- demo/generateTTS.py
+++ demo/generateTTS.py
@@ -1,13 +1,25 @@
 from elevenlabslib.helpers import *
 from elevenlabslib import *
-
+from elevenlabs import *
 # A script used to generate MP3 TTS files
 
-api_key = "ecf8b902a86fab1c3ec866b9a8ed6fc3"
+api_key = "652cfcc78a55462e2d05ea895c3625ae"
 user = ElevenLabsUser(api_key)
-premadeVoice = user.get_voices_by_name("Jarvis")[0]
+premadeVoice = user.get_voices_by_name("British")[0]
 
-text = "For example, I can play you music or podcasts from Spotify!... Let me play you a song by Queen."
+text = "Let me show you my walk!"
 
 generationData = premadeVoice.generate_play_audio(text , stability=0.1, similarity_boost=0.3, playInBackground=False, latencyOptimizationLevel = 0)
 save_audio_bytes(generationData[0], "sample.mp3", outputFormat="mp3")
+
+# FUNCTION FOR DEMO:
+# def speak(text, auto_play=True):
+#     print('K9: ' + text)
+#     set_api_key("652cfcc78a55462e2d05ea895c3625ae")
+#     available_voices = voices()
+#     audio = generate(
+#       text=text,
+#       voice=available_voices[9],
+#       model="eleven_monolingual_v1"
+#     )
+#     save(audio, "tts.mp3")
